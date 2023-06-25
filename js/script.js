041dd7c5acd2d8151da1d95bec1a7973d5bdba3d
David Forrester <david.o.forrester@gmail.com>
2023-06-25 20:57:50 +1000

    adding the temp from the api to the dom

--- js/script.js
+++ js/script.js
@@ -8,6 +8,16 @@ async function getWeather() {
   const response = await fetch(
     "https://api.weatherapi.com/v1/current.json?key=021675ea36b44361b9c73259232506&q=london"
   );
-  const weather = await response.json()
-  console.log(weather);
+  const weather = await response.json();
+  const current = await weather.current;
+  const tempC = await current.temp_c;
+  console.log(tempC);
+  updateDom(tempC)
+}
+
+function updateDom (temp) {
+  const tempDiv = document.createElement('div')
+  tempDiv.textContent = temp
+
+  document.body.appendChild(tempDiv)
 }
