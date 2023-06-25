ec583bae609576d9ed36659ef136b034167ddf8c
David Forrester <david.o.forrester@gmail.com>
2023-06-25 20:51:41 +1000

    returned the json from the weather api

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-// Variables
+const getWeatherButton = document.getElementById("get-weather");
 
+getWeatherButton.addEventListener("click", () => {
+  getWeather();
+});
 
-// Code body
-
-
-// Functions
-
+async function getWeather() {
+  const response = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=021675ea36b44361b9c73259232506&q=london"
+  );
+  const weather = await response.json()
+  console.log(weather);
+}
