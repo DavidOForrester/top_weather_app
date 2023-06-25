df58fef6e36acad68106b6395c93440d5999a81c
David Forrester <david.o.forrester@gmail.com>
2023-06-25 21:09:34 +1000

    implimenting error checking

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
 const getWeatherButton = document.getElementById("get-weather");
 
 getWeatherButton.addEventListener("click", () => {
-  const inputLocation = document.getElementById('location')
+  const inputLocation = document.getElementById("location");
   const location = inputLocation.value;
   getWeather(location);
 });
 
 async function getWeather(location) {
-  const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=021675ea36b44361b9c73259232506&q=" +
-      location
-  );
-  const weather = await response.json();
-  const current = await weather.current;
-  const tempC = await current.temp_c;
-  console.log(tempC);
-  updateDom(tempC);
+  try {
+    const response = await fetch(
+      "https://api.weatherapi.com/v1/current.json?key=021675ea36b44361b9c73259232506&q=" +
+        location
+    );
+    const weather = await response.json();
+    const current = await weather.current;
+    const tempC = await current.temp_c;
+    updateDom(tempC);
+  } catch(error) {
+    console.log(error)
+  }
 }
 
 function updateDom(temp) {
