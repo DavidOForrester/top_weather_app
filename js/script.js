c16b94982b8d64d74ddcf721545a9178877ff667
David Forrester <david.o.forrester@gmail.com>
2023-06-25 21:05:37 +1000

    user can now enter location

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 const getWeatherButton = document.getElementById("get-weather");
 
 getWeatherButton.addEventListener("click", () => {
-  getWeather();
+  const inputLocation = document.getElementById('location')
+  const location = inputLocation.value;
+  getWeather(location);
 });
 
-async function getWeather() {
+async function getWeather(location) {
   const response = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=021675ea36b44361b9c73259232506&q=london"
+    "https://api.weatherapi.com/v1/current.json?key=021675ea36b44361b9c73259232506&q=" +
+      location
   );
   const weather = await response.json();
   const current = await weather.current;
   const tempC = await current.temp_c;
   console.log(tempC);
-  updateDom(tempC)
+  updateDom(tempC);
 }
 
-function updateDom (temp) {
-  const tempDiv = document.createElement('div')
-  tempDiv.textContent = temp
+function updateDom(temp) {
+  const tempDiv = document.createElement("div");
+  tempDiv.textContent = temp;
 
-  document.body.appendChild(tempDiv)
+  document.body.appendChild(tempDiv);
 }
